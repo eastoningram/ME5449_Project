e6c9902475383443489eae194ccb42bd5e7b5d8c
Easton Ingram <eastoningram@gmail.com>
2024-04-17 17:40:38 -0500

    plz dont explode

--- main.py
+++ main.py
@@ -5,31 +5,38 @@ from datetime import datetime
 import numpy as np
 import time
 from PIL import Image #Image splicing
+import RPi.GPIO as GPIO
+from rdp import rdp # Ramer-Douglas-Peucker Algorithm
+import matplotlib.pyplot as plt # graphing tool
 
 from funcs import *
 rows=128
 cols=128
 
-n_pics=2
+x_frames=2
+y_frames=2
+
+switch=26 # Input from robot
+
+frame = np.empty((rows, cols, 3), dtype=np.uint8)
+output = np.empty((rows*x_frames, cols*y_frames, 3), dtype=np.uint8)
 
 #Take picture
 with picamera.PiCamera() as camera:
     camera.resolution = (rows, cols)
     camera.framerate = 24
-    camera.start_preview()
-    time.sleep(2)
-    output1 = np.empty((rows, cols, 3), dtype=np.uint8)
-    output2 = np.empty((rows, cols, 3), dtype=np.uint8)
-    camera.capture(output1, 'rgb') # Get array
-    print("Move to position 2, then hit enter")
-    input()
-    camera.capture(output2, 'rgb') # Get array
-    camera.capture('capture1.jpg') # Make image
-    camera.capture('capture2.jpg') # Make image
-    
-    output = np.empty((rows, cols*2, 3), dtype=np.uint8)
-    output[:,:cols,:]=output1
-    output[:,cols:,:]=output2
+
+    for j in range(y_frames):
+        for i in range(x_frames):
+
+            # wait for input from robot
+            while not GPIO.input(switch):
+                time.sleep(0.01)
+            
+            camera.capture(frame, 'rgb') # Grab array
+            output[j*rows:(j+1)*rows,i*cols:(1+i)*cols,:]=frame # Splice images together
+            camera.capture('capture_'+str(i) + '_' + str(j) + '.jpg') # Make image
+            print('Took x= ' +str(i)+', y= '+ str(j))
 
     image = Image.fromarray(output.astype('uint8')).convert('RGB')
     image.save('splice.jpg')
@@ -56,16 +63,31 @@ for r in range(rows):
         print(black_and_white[r,c] ,end= ' ')
     print()
 
+points=[1000, 2]
+n_points=0
 
 print('_'*cols)
 for r in range(rows):
     for c in range(cols):
         if (black_and_white[r, c] <  100):
             print('.' ,end='')
+            points[n_points]=[c,r]
         else:
             print(' ',end='')
     print('|')
 
 print('_'*cols)
+
+# Extract x and y coordinates from the points
+x_values = [point[0] for point in points]
+y_values = [point[1] for point in points]
+
+# Plot the points
+plt.scatter(x_values, y_values)
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Plot of Points')
+plt.grid(True)
+plt.show()
 
 
